62b3d5f65d391a1d1bd471d15aeb6c267a9df52b
Jorenios PC <babyteam.vg@gmail.com>
2026-02-17 14:03:33 -0300

    greg Anim FIx

--- Assets/Scripts/LevelControllers/MusicLevelController.cs
+++ Assets/Scripts/LevelControllers/MusicLevelController.cs
@@ -10,7 +10,7 @@ public class MusicLevelController : MonoBehaviour
     //public int currentSong { get; private set; } = 0;
     [SerializeField] private SongsData levelSongData;
     
-    
+    /*
     private void OnEnable()
     {
         AudioManager.OnPlay += OnPlayEvent;
@@ -22,19 +22,16 @@ public class MusicLevelController : MonoBehaviour
         AudioManager.OnPlay -= OnPlayEvent;
         AudioManager.OnStop -= OnStopEvent;
     }
+	*/
+
     public void Start()
     {
-        //BeatManager.Instance.SetAudioSource(allSongs[currentSong],BeatManager.Instance.CurrentAudioSourceIndex);
-        //BeatManager.Instance.SetAudioSource(allSongs[currentSong+1],BeatManager.Instance.CurrentAudioSourceIndex+1);
-        if(shouldStartPlaying && !AudioManager.Instance.IsPlaying()) PlayLevelSong();
-        
+        if(shouldStartPlaying && !AudioManager.Instance.IsPlaying()) PlayLevelSong();        
     }
 
     public void PlayLevelSong(int index=0)
     {
         AudioManager.Instance.PlaySongData(levelSongData);
-        
-
     }
     
     private void OnPlayEvent()
