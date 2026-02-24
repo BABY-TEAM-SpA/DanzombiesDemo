34a0381e6f210b38ba2d143ce0b6242cfa910964
Jorenios PC <babyteam.vg@gmail.com>
2026-02-24 19:26:00 -0300

    AudioManager Fix Loop

--- Assets/ScriptableObject/SongDataSO.cs
+++ Assets/ScriptableObject/SongDataSO.cs
@@ -6,7 +6,13 @@ public class SongDataSO : ScriptableObject
 {
     public string songName;
     public AudioClip clip;
-    public int bpm;
     public bool loopeable;
-    public List<int> cutFlags = new List<int>();
+    public List<int> cutFlags = new List<int>(); // in compases
+    public double delayAtEnd = 0d;
+    
+    [Header("Music Data")]
+    public int bpm;
+    public int metric;
+    public int compassLong;
+
 }
