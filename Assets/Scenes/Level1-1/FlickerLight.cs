ba743b866ed5bffd063190ec9bac11b356f3e741
irith14 <feravila.fga@gmail.com>
2026-02-25 00:02:09 -0300

    Pref Lvl1-1 and Beautiful code make light flicker

---
+++ Assets/Scenes/Level1-1/FlickerLight.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.U2D;
+
+public class FlickerLight : MonoBehaviour
+{
+    [SerializeField, Min(0f)] private float minIntensity = 0.5f;
+    [SerializeField, Min(0f)] private float maxIntensity = 1.2f;
+    [SerializeField, Min(0f)] private float timeBetweenIntensity = 0.1f;
+
+    private Light2D lightToFlicker;
+    private float currentTimer;
+
+    private void OnValidate()
+    {
+        maxIntensity = Mathf.Max(maxIntensity, minIntensity);
+    }
+    private void Awake()
+    {
+        if (lightToFlicker == null)
+        {
+            lightToFlicker = GetComponent<Light2D>();
+        }
+        
+    }
+
+    private void Update()
+    {
+        currentTimer+= Time.deltaTime;
+        if (!(currentTimer >= timeBetweenIntensity)) return;
+        lightToFlicker.intensity = Random.Range(minIntensity, maxIntensity);
+        currentTimer = 0;
+
+    }
+}
