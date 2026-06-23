2c0d51e03deb965111747dd16bdd7db442c2d848
Franco <fmss2000@gmail.com>
2026-06-23 13:55:00 -0400

    PlayerTriggeredCamera and Confiners
    New prefab for the cameras, cleaning hierarchy and positions/offsets. CinemachineConfiner was also implemented for the HitAndRun scene.

---
+++ Assets/Danzombies/Developer/Prototypes/HitAndRun/PlayerTriggeredCamera.cs
@@ -0,0 +1,36 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class PlayerTriggeredCamera : MonoBehaviour
+{
+    #region [VARIABLES]
+    private const int ACTIVE_PRIORITY = 1;
+    private const int INACTIVE_PRIORITY = 0;
+
+    [SerializeField] private CinemachineStateDrivenCamera stateDrivenCamera;
+    #endregion
+
+    #region [UNITY]
+    private void Start() => stateDrivenCamera.Priority = INACTIVE_PRIORITY;
+
+    #region Trigger
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        if (other.TryGetComponent<PlayerManager>(out PlayerManager playerManager))
+            stateDrivenCamera.Priority = ACTIVE_PRIORITY;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        if (other.TryGetComponent<PlayerManager>(out PlayerManager playerManager))
+            stateDrivenCamera.Priority = INACTIVE_PRIORITY;
+    }
+    #endregion
+    #endregion
+}
