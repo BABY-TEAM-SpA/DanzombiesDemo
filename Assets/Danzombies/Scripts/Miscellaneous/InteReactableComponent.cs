4c4f782516858e7b3965a3413575538aac069575
Franco <fmss2000@gmail.com>
2026-07-22 15:37:06 -0400

    InteReactable fix
    xd

--- Assets/Danzombies/Scripts/Miscellaneous/InteReactableComponent.cs
+++ Assets/Danzombies/Scripts/Miscellaneous/InteReactableComponent.cs
@@ -197,12 +197,12 @@ public class InteReactableComponent : MonoBehaviour, IResettable
                 $"Siempre es preferible que áreas distintas (e.g. Reactable " +
                 $"e Interactable) estén en GameObjects independientes.", this);
 
-        if (collider.TryGetComponent<InteReactableArea>(out InteReactableArea area))
-        {
-            area.OnEntered += HandleAreaEntered;
-            area.OnExited += HandleAreaExited;
-            area.Setup(type);
-        }
+        if (!collider.TryGetComponent<InteReactableArea>(out InteReactableArea area))
+            area = collider.gameObject.AddComponent<InteReactableArea>();
+
+        area.OnEntered += HandleAreaEntered;
+        area.OnExited += HandleAreaExited;
+        area.Setup(type);
     }
 
     private void HandleEventTrigger(InteReaction.InteReactionEvent e)
