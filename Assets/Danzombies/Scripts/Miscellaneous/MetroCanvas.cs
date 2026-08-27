7a36de522c16c5a2dbb4e685386038e6eedee9b5
Frco <frco.indie@gmail.com>
2026-08-26 21:07:19 -0400

    Fixes a la escena 1.1.2
    Fix de la Timeline cuando se bajaba Greg, las hordas ya no deberían teletransportarse y se dejó preparado el Canvas para dar feedback para el Shift+D.

--- Assets/Danzombies/Scripts/Miscellaneous/MetroCanvas.cs
+++ Assets/Danzombies/Scripts/Miscellaneous/MetroCanvas.cs
@@ -1,25 +1,44 @@
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
+[System.Serializable]
+public class InputStateTracker
+{
+    [SerializeField] private UnityEvent onPressed;
+    [SerializeField] private UnityEvent onReleased;
+    private bool state;
+
+    public bool Check(bool currentState)
+    {
+        if (state != currentState)
+        {
+            if (currentState) onPressed?.Invoke();
+            else onReleased?.Invoke();
+            state = currentState;
+        }
+        return currentState;
+    }
+}
+
 public class MetroCanvas : MonoBehaviour
 {
-    #region [VARIABLES]
     [SerializeField] private InputActionReference sprintRef;
+    [SerializeField] private InputStateTracker sprintTracker;
+
     [SerializeField] private InputActionReference moveRef;
+    [SerializeField] private InputStateTracker moveTracker;
 
-    public UnityEvent OnShiftPlusD;
-    #endregion
+    public UnityEvent OnSprintPlusMove;
 
-    #region [UNITY]
     private void LateUpdate()
     {
-        bool shift = sprintRef.action.WasPressedThisFrame();
-        bool d = moveRef.action.ReadValue<Vector2>().x > 0;
+        bool sprinting = sprintTracker.Check(sprintRef.action.IsPressed());
+        bool moving = moveTracker.Check(moveRef.action.ReadValue<Vector2>().x > 0);
 
-        if (shift && d)
-            OnShiftPlusD?.Invoke();
+        if (sprinting && moving)
+            OnSprintPlusMove?.Invoke();
     }
-    #endregion
 }
